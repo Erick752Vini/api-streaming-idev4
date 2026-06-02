b3b6fb74f7de089467af4f0bddb6713ed199085f
Erick752Vini <erickviniciusrocha80@gmail.com>
2026-06-02 14:47:41 -0300

    adicionando post

--- routes/livros.js
+++ routes/livros.js
@@ -1,7 +1,58 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const router = express.Router();
 
+const livrosSchema = new mongoose.Schema({
+  titulo: String,
+  autor: String,
+  genero: String,
+  ano: Number,
+  paginas: Number,
+  disponivel: Boolean,
+  detalhes: Object,
+});
 
+const Livro = mongoose.model('livros', livrosSchema);
+
+router.get('/', async (req, res) => {
+    try {
+        const livros = await Livro.find();
+        res.json(livros);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const novoLivro = new Livro(req.body);
+        await novoLivro.save();
+        res.status(201).json(novoLivro);
+    } catch (error) {
+        res.status(400).json({
+            message: error.message,
+        });
+    }
+});
+
+router.put('/', async (req, res) => {
+    try{
+        const livroAtualizado = await Livro.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true},
+        );
+        if(!livroAtualizado) {
+            return res.status(404).json({
+                message: "Livro não encontrado",
+            });
+        }
+        res.status(200).json(livroAtualizado);
+    } catch (error) {
+        res.status(400).json({
+            message: error.message,
+        });
+    }
+});
 
 module.exports = router;
--- routes/series.js
+++ routes/series.js
@@ -2,6 +2,58 @@ const express = require('express');
 const mongoose = require('mongoose')
 const router = express.Router();
 
+const seriesSchema = new mongoose.Schema({
+    titulo: String,
+    genero: String,
+    ano: Number,
+    nota: Number,
+    disponivel: Boolean,
+    temporadas: Number,
+    atores: Array,
+    detalhes: Object
+});
 
+const Series = mongoose.model('series', seriesSchema);
+
+router.get('/', async (req, res) => {
+    try {
+        const series = await Series.find();
+        res.json(series);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try{
+    const novaSerie = new Series(req.body);
+    await novaSerie.save();
+    res.status(201).json(novaSerie);
+    } catch (error) {
+        res.status(400).json({
+            message: error.message,
+        });
+    }
+});
+
+router.put('/', async (req, res) => {
+    try{
+        const serieAtualizada = await Series.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true},
+        );
+        if(!serieAtualizada) {
+            return res.status(404).json({
+                message: "Série não encontrada",
+            });
+        }
+        res.status(200).json(serieAtualizada);
+    } catch (error) {
+        res.status(400).json({
+            message: error.message,
+        });
+    }
+});
 
 module.exports = router;
